4fd36ba0d6ad74e00654a3f57fe9c02f71c43fb1
cristiancho29 <cristiancho29@gmail.com>
2018-03-26 11:57:33 -0500

    Update index.js

--- api/v1/index.js
+++ api/v1/index.js
@@ -3,8 +3,6 @@
 const router = require("express").Router();
  
 const usersRoutes = require("./routes/users");
-const messagesRoutes = require("./routes/messages");
 router.use("/users", usersRoutes);
-router.use("/messages",messagesRoutes);
 
 module.exports = router;

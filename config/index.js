18e9aa593b4e3ad9a520f72ce4b204d1f4e34810
cristiancho29 <cdsuarez@uninorte.edu.co>
2018-04-04 14:13:34 +0000

    config

--- config/index.js
+++ config/index.js
@@ -1,12 +1,14 @@
 "use strict";
-
+//Nombre db: labdb
+//Activar mongodb /usr/bin/mongod --dbpath=data --port=27017 --nojournal 2>&1 &
+//Repair mongodb /usr/bin/mongod --dbpath=data --repair
 const config = {
     hostname: "127.0.0.1",
     port: 27017,
     db: {
-        url: "mongodb://localhost/MyApp"
+        url: "mongodb://localhost/labdb"
     },
-    secret: process.env.SECRET_KEY,
+    secret: "prueba123",
 };
 
 module.exports = config;

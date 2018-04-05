f0ae8eaf612a9d4bc6871d9e95fa713ab61e138c
cristiancho29 <cdsuarez@uninorte.edu.co>
2018-04-05 16:57:18 +0000

    --

--- api/v1/models/ensayos.js
+++ api/v1/models/ensayos.js
@@ -3,6 +3,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+mongoose.Promise = global.Promise;
+
 const EnsayoModel = new Schema({
     campos:{
         type:String,
--- api/v1/models/tipoensayos.js
+++ api/v1/models/tipoensayos.js
@@ -3,6 +3,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+mongoose.Promise = global.Promise;
+
 const TipoEnsayoModel = new Schema({
     name:{
         type:String,

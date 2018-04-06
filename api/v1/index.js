8cc5514b94d35b165e85fadc9956e95531b8bda3
cristiancho29 <cdsuarez@uninorte.edu.co>
2018-04-06 00:13:11 +0000

    --

--- api/v1/index.js
+++ api/v1/index.js
@@ -6,7 +6,7 @@ const usersRoutes = require("./routes/users");
 const ensayosRoutes = require("./routes/ensayos");
 const tipoensayosRoutes = require("./routes/tipoensayos");
 router.use("/users", usersRoutes);
-router.use("/ensayos", usersRoutes);
-router.use("/tipoensayos", usersRoutes);
+router.use("/ensayos", ensayosRoutes);
+router.use("/tipoensayos", tipoensayosRoutes);
 
 module.exports = router;

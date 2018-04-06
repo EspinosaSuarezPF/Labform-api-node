28eb8f03886365b31f27ff92879920106e58a692
cristiancho29 <cdsuarez@uninorte.edu.co>
2018-04-06 00:11:11 +0000

    --

--- api/v1/routes/tipoensayos.js
+++ api/v1/routes/tipoensayos.js
@@ -4,12 +4,12 @@ const router = require("express").Router();
 const controller = require("./../controllers/tipoensayos");
 
 router.route("/")
-/*
+
 .get()
 .post()
 .put()
 .delete();
-*/
+
 
 router.route("/:id")
     .get()
